03f375d48e64d8a22d21042acd8ce4e8d099ed35
Naotarouu <darrencorpuz957@gmail.com>
2024-01-30 17:23:05 +0800

    update dashboard

--- apps/screens/Dashboard.tsx
+++ apps/screens/Dashboard.tsx
@@ -98,26 +98,26 @@ const Dashboard = () => {
         <View style={{ alignItems: 'center', borderRadius: 25 }}>
           <View style={{ flexDirection: "row", marginTop: 20, gap: 15 }}>
             <View style={{ height: 210, borderWidth: 2, borderColor: "gray", borderRadius: 25, padding: 15, alignSelf: "center", width: 160, backgroundColor: "#91e8fa" }}>
-              <View style={{ alignSelf: "center", paddingTop: 20 }}>
-                <Image style={{ width: 80, height: 80 }} source={{ uri: 'https://cdn.discordapp.com/attachments/1194934283433943050/1197798223906082816/kisspng-poor-posture-human-back-low-back-pain-middle-back-old-how-it-works-study-in-australia-information-5b716872143556.7840094615341589620828.png?ex=65bc9386&is=65aa1e86&hm=745f6e11519e89b3f6339e97dce09f359114b0f15acab04ed550c1896c1a2dc9&' }} />
+              <View style={{ alignSelf: "center" }}>
+                <Image style={{ width: 50, height: 50 }} source={{ uri: 'https://cdn.discordapp.com/attachments/1194934283433943050/1197798223906082816/kisspng-poor-posture-human-back-low-back-pain-middle-back-old-how-it-works-study-in-australia-information-5b716872143556.7840094615341589620828.png?ex=65bc9386&is=65aa1e86&hm=745f6e11519e89b3f6339e97dce09f359114b0f15acab04ed550c1896c1a2dc9&' }} />
               </View>
               <View style={{ alignSelf: "center" }}>
                 <Text style={{ alignSelf: "center" }}>Posture</Text>
                 <Text style={{ alignSelf: "center" }}>{postureRating}/10</Text>
-                <Text style={{ alignSelf: "center", padding: 15, justifyContent: "center", textAlign: "center" }}>
+                <Text style={{ alignSelf: "center", padding: 10, justifyContent: "center", textAlign: "center" }}>
                   {postureComment}
                 </Text>
               </View>
             </View>
 
             <View style={{ height: 210, borderWidth: 2, borderColor: "gray", borderRadius: 25, padding: 15, alignSelf: "center", width: 160, backgroundColor: "#91e8fa" }}>
-              <View style={{ alignSelf: "center", paddingTop: 20 }}>
-                <Image style={{ width: 80, height: 80 }} source={{ uri: 'https://cdn.discordapp.com/attachments/1194934283433943050/1197838179726798878/Temperature.png?ex=65bcb8bc&is=65aa43bc&hm=df8c2c870700caee8f1e9335eb03a4e31b371c239b0fd309c3ed1306789fa6f5&' }} />
+              <View style={{ alignSelf: "center"}}>
+                <Image style={{ width: 50, height: 50 }} source={{ uri: 'https://cdn.discordapp.com/attachments/1194934283433943050/1197838179726798878/Temperature.png?ex=65bcb8bc&is=65aa43bc&hm=df8c2c870700caee8f1e9335eb03a4e31b371c239b0fd309c3ed1306789fa6f5&' }} />
               </View>
               <View style={{ alignSelf: "center", alignItems: "center" }}>
                 <Text>Temperature</Text>
                 <Text>{temperatureRating}°C</Text>
-                <Text style={{ alignSelf: "center", padding: 15, justifyContent: "center", textAlign: "center" }}>
+                <Text style={{ alignSelf: "center", padding: 10, justifyContent: "center", textAlign: "center" }}>
                   {temperatureComment}
                 </Text>
               </View>
@@ -126,26 +126,26 @@ const Dashboard = () => {
 
           <View style={{ flexDirection: "row", marginTop: 20, marginBottom: 30, gap: 15 }}>
             <View style={{ height: 210, borderWidth: 2, borderColor: "gray", borderRadius: 25, padding: 15, alignSelf: "center", width: 160, backgroundColor: "#91e8fa" }}>
-              <View style={{ alignSelf: "center", paddingTop: 20 }}>
-                <Image style={{ width: 80, height: 80 }} source={{ uri: 'https://media.discordapp.net/attachments/1194934283433943050/1197836883586195496/Loudness.png?ex=65bcb787&is=65aa4287&hm=f5fe1850d7851ef2987a823d1c991692a6a60f09d1948f7bb058efb5fb6218aa&=&format=webp&quality=lossless&width=640&height=640' }} />
+              <View style={{ alignSelf: "center" }}>
+                <Image style={{ width: 50, height: 50 }} source={{ uri: 'https://media.discordapp.net/attachments/1194934283433943050/1197836883586195496/Loudness.png?ex=65bcb787&is=65aa4287&hm=f5fe1850d7851ef2987a823d1c991692a6a60f09d1948f7bb058efb5fb6218aa&=&format=webp&quality=lossless&width=640&height=640' }} />
               </View>
               <View style={{ alignSelf: "center", alignItems: "center" }}>
                 <Text>Loudness</Text>
                 <Text>{loudnessRating}dB</Text>
-                <Text style={{ alignSelf: "center", padding: 15, justifyContent: "center", textAlign: "center" }}>
+                <Text style={{ alignSelf: "center", padding: 10, justifyContent: "center", textAlign: "center" }}>
                   {loudnessComment}
                 </Text>
               </View>
             </View>
 
             <View style={{ height: 210, borderWidth: 2, borderColor: "gray", borderRadius: 25, padding: 15, alignSelf: "center", width: 160, backgroundColor: "#91e8fa" }}>
-              <View style={{ alignSelf: "center", paddingTop: 20 }}>
-                <Image style={{ width: 80, height: 80 }} source={{ uri: 'https://media.discordapp.net/attachments/1194934283433943050/1197836883321962558/Humidity.png?ex=65bcb787&is=65aa4287&hm=c9897fa676ab889acf3e2b7276b60ed8765bfd55d1c5eb08a13b79bcf67350e4&=&format=webp&quality=lossless&width=640&height=640' }} />
+              <View style={{ alignSelf: "center" }}>
+                <Image style={{ width: 50, height: 50 }} source={{ uri: 'https://media.discordapp.net/attachments/1194934283433943050/1197836883321962558/Humidity.png?ex=65bcb787&is=65aa4287&hm=c9897fa676ab889acf3e2b7276b60ed8765bfd55d1c5eb08a13b79bcf67350e4&=&format=webp&quality=lossless&width=640&height=640' }} />
               </View>
               <View style={{ alignSelf: "center", alignItems: "center" }}>
                 <Text>Humidity</Text>
                 <Text>{humidityRating}%</Text>
-                <Text style={{ alignSelf: "center", padding: 15, justifyContent: "center", textAlign: "center" }}>
+                <Text style={{ alignSelf: "center", padding: 10, justifyContent: "center", textAlign: "center" }}>
                   {humidityComment}
                 </Text>
               </View>
